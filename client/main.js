77462b2c17e68b92a08a2d2b836214cd273d80ef
Volodymyr Vozniak <volvoz@quintagroup.org>
2016-06-29 16:15:10 +0300

    added ethereum base packages

--- client/main.js
+++ client/main.js
@@ -6,12 +6,16 @@ import './main.html';
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
+  EthBlocks.init();
 });
 
 Template.hello.helpers({
   counter() {
     return Template.instance().counter.get();
   },
+  currentBlock: function(){
+        return EthBlocks.latest.number;
+    }
 });
 
 Template.hello.events({
